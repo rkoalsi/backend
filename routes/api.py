26b0da30de810f3fb1b88978d8776ce221c2725b
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2025-01-21 09:57:35 +0530

    changing sales peoples routes

--- routes/api.py
+++ routes/api.py
@@ -6,12 +6,12 @@ from .zoho import router as zoho
 from .orders import router as orders
 from .util import router as util
 from .admin import router as admin
+from .salespeople import router as salespeople
 from .webhooks import router as webhooks
 
 router = APIRouter()
 
 router.include_router(users, prefix="/users", tags=["User"])
-print(router.routes)
 router.include_router(customers, prefix="/customers", tags=["Customer"])
 router.include_router(products, prefix="/products", tags=["Product"])
 router.include_router(zoho, prefix="/zoho", tags=["Zoho"])
@@ -19,6 +19,7 @@ router.include_router(orders, prefix="/orders", tags=["Orders"])
 router.include_router(admin, prefix="/admin", tags=["Admin"])
 router.include_router(util, prefix="/util", tags=["Util"])
 router.include_router(webhooks, prefix="/zoho/webhooks", tags=["Zoho"])
+router.include_router(salespeople, prefix="/salespeoples", tags=["SalesPeople"])
 
 
 @router.get("/")
---
+++ routes/salespeople.py
@@ -0,0 +1,80 @@
+from fastapi import APIRouter, HTTPException
+from backend.config.root import connect_to_mongo, serialize_mongo_document, parse_data  # type: ignore
+from bson import ObjectId
+
+router = APIRouter()
+
+client, db = connect_to_mongo()
+
+
+client, db = connect_to_mongo()
+users_collection = db["users"]
+
+
+@router.get("")
+def salespeople():
+    users_cursor = db.users.find({"role": "sales_person"})
+    sales_people = list(users_cursor)
+
+    # Prepare the result
+    for sales_person in sales_people:
+        sales_person_code = sales_person.get("code")
+
+        if sales_person_code:
+            # Fetch customers assigned to the salesperson
+            customers_cursor = db.customers.find(
+                {
+                    "$or": [
+                        {
+                            "cf_sales_person": {
+                                "$regex": f"\\b{sales_person_code}\\b",
+                                "$options": "i",
+                            }
+                        },
+                        {"cf_sales_person": "Defaulter"},
+                        {"cf_sales_person": "Company customers"},
+                    ],
+                    "status": "active",
+                }
+            )
+            sales_person["customers"] = serialize_mongo_document(list(customers_cursor))
+        else:
+            # Assign customers with "Defaulter" or "Company customers" to all salespeople
+            customers_cursor = db.customers.find(
+                {
+                    "$or": [
+                        {"cf_sales_person": "Defaulter"},
+                        {"cf_sales_person": "Company customers"},
+                    ],
+                    "status": "active",
+                }
+            )
+            sales_person["customers"] = serialize_mongo_document(list(customers_cursor))
+
+    return {"users": serialize_mongo_document(sales_people)}
+
+
+@router.get("/customers")
+def get_salespeople_customers():
+    users_cursor = db.users.find({"role": "sales_person"})
+    users = serialize_mongo_document(list(users_cursor))
+    return {"users": users}
+
+
+@router.put("/{salesperson_id}")
+def salespeople_id(salesperson_id: str, salesperson: dict):
+    update_data = {k: v for k, v in salesperson.items() if k != "_id" and v is not None}
+
+    if not update_data:
+        raise HTTPException(
+            status_code=400, detail="No valid fields provided for update"
+        )
+
+    # Perform the update
+    result = db.users.update_one(
+        {"_id": ObjectId(salesperson_id)},
+        {"$set": update_data},
+    )
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Sales Person not found")
+    return {"message": "Sales Person Updated"}
