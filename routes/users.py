a411062f90d80a2c54f7c9e3e5722a5ccfe63f84
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2025-01-28 10:46:38 +0530

    removing ACCESS_TOKEN_EXPIRE_MINUTES

--- routes/users.py
+++ routes/users.py
@@ -29,7 +29,7 @@ FRONTEND_RESET_URL = os.getenv("FRONTEND_RESET_URL")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
