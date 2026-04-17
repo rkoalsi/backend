2da244eaa7111ae1d3fff014e782b345f8d375fe
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2026-04-17 15:04:20 +0530

    fixing credit note download for return order

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import logging
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from .routes.api import router
 from .config.root import get_database, disconnect_on_exit
@@ -91,7 +91,9 @@ async def handle_options():
 
 
 @app.exception_handler(404)
-async def custom_404_handler(_, __):
+async def custom_404_handler(request: Request, __):
+    if request.url.path.startswith("/api/"):
+        return JSONResponse({"detail": "Not Found"}, status_code=404)
     return RedirectResponse("/")
 
 
